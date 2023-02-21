11b554025f7e9bf23ac150406ebacde916542146
Himanshi Jangid <himanshijangid444@gmail.com>
2023-02-21 14:17:00 +0530

    files added

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const amount = document.getElementById('amount');
+const guest = document.getElementById('guest');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+calculate = () => {
+    const tip= ((amount.value*quality.value)/guest.value);
+    amount.value = "";
+    guest.value = "";
+    quality.value = "";
+    if(tip=== 'NaN'){
+        tipAmount.innerHTML='please insert a number';
+        showTipAmount();
+
+    }else{
+        tipAmount.innerHTML= 'tip &#8377;'+ tip + 'each';
+        showTipAmount();
+    }
+
+    }
+    showTipAmount = () => {
+    tipAmount.className = "show";
+    setTimeout(function(){
+        tipAmount.className= tipAmount.className.replace('show', "");
+    }, 6000);
+            
+
+    }
+
 
